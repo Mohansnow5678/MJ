4f060c16a51196713662fe10c720149251605d7e
mohanasundaram.v <mohanasundaram.v@DT150131.QICHOAPPL.com>
2022-04-29 10:30:11 +0530

    codes

--- code/src/main/java/code/Doctor.java
+++ code/src/main/java/code/Doctor.java
@@ -1,12 +1,18 @@
 package code;
 
-public class Simpleproject {
+public class Doctor {
 
 	public static void main(String[] args) {
-		
-		System.err.println("hii");
 		// TODO Auto-generated method stub
 
+		
+		
+		
+		int a=10;
+		
+	System.out.println("a");
+		
+		
 	}
 
 }
