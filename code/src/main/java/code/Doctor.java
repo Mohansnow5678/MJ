b8e48068419403ddc2c7fa19166ea25da347ca29
mohanasundaram.v <mohanasundaram.v@DT150131.QICHOAPPL.com>
2022-05-04 15:19:31 +0530

    success

--- code/src/main/java/code/Doctor.java
+++ code/src/main/java/code/Doctor.java
@@ -1,18 +1,41 @@
+
+
+
 package code;
+
+
 
 public class Doctor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+  String fr="Float";
+  String ft="Float1";
+  int sc=12;
+  String Ag="Age";
+  String tr="Wharehouse";
+  String Wh="Wh1";
+  
+  String temp = fr;
+  fr=ft;
+  ft=temp;
+  
+  String cv=Integer.toString(sc);
+  
+  String sr =cv;
+  cv=Ag;
+  Ag=sr;
+  
+  String hn=tr;
+  tr=Wh;
+  Wh=hn;
+  
+  String Result=("\""+fr+":"+ft+"/"+sc+":"+Ag+"/"+tr+":"+Wh+"\"");
+  
+  System.out.println(Result.toUpperCase());
+  
+     
+		  }
+		}
 
-		
-		
-		
-		int a=10;
-		
-	System.out.println("a");
-		
-		
-	}
-
-}
+	
--- element/src/main/java/element/Number.java
+++ element/src/main/java/element/Number.java
@@ -5,8 +5,34 @@ public class Number {
 	public static void main(String[] args) {
 		
 		
-	System.out.println("series fun");	
-		kaliyapermal
-	}
+	
+				int i,j, row = 6;       
+	
+		for (i=0; i<row; i++)   
+		{  
+    
+		for (j=row-i; j>1; j--)   
+		{  
+		
+		System.out.print(" ");   
+		}   
+ 
+		for (j=0; j<=i; j++ )   
+		{   
+	    
+		System.out.print("**");   
 
 }
+
+		System.out.println();   
+
+		
+		}
+	}
+		
+		
+		
+		
+		
+		
+		}
